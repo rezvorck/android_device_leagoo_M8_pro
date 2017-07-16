5c7ea05d1ab652b0dbf48a1a6bfecc4931ea2e23
hyperion70 <bmaximov71@gmail.com>
2017-07-16 22:57:52 +0300

    Add some  configs

--- full_M8_pro.mk
+++ full_M8_pro.mk
@@ -55,6 +55,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
     $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml
 
+# Media	
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
 # Prebuilt
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/bin/fingerprintd:system/bin/fingerprintd \
