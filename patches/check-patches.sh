222c6d4596a57c551ae74f1db6f892ead43126cf
hyperion70 <bmaximov71@gmail.com>
2017-07-24 21:56:55 +0300

    Fix battery voltage for MTK

--- patches/check-patches.sh
+++ patches/check-patches.sh
@@ -4,6 +4,7 @@ cd system/core
 git apply -v --check ../../device/leagoo/M8_pro/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 git apply -v --check ../../device/leagoo/M8_pro/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 git apply -v --check ../../device/leagoo/M8_pro/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
+git apply -v --check ../../device/leagoo/M8_pro/patches/0020-healthd_batteryVoltage.patch
 cd ../..
 cd bionic
 git apply -v --check ../device/leagoo/M8_pro/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
